4e54678aefcb5cd8de3937f1ab7ab06dcfa30b03
Etienne Dauvergne <contact@ekyna.com>
2015-10-12 13:54:34 +0200

    [PayumSipsBundle] Fix configuration and factory, Client as service, Payment methods image.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -2,6 +2,7 @@
 
 namespace Ekyna\Bundle\PayumSipsBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -18,29 +19,37 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ekyna_payum_sips');
 
-        $rootNode
-            ->addDefaultsIfNotSet()
-            ->children()
-                ->scalarNode('merchant_id')->isRequired()->cannotBeEmpty()->end()
-                ->scalarNode('merchant_country')->isRequired()->cannotBeEmpty()->end()
-                ->scalarNode('pathfile')
-                    ->cannotBeEmpty()
-                    ->defaultValue('%kernel.root_dir%/config/sips/param/pathfile')
-                ->end()
-                ->scalarNode('request_bin')
-                    ->cannotBeEmpty()
-                    ->defaultValue('%kernel.root_dir%/config/sips/bin/static/request')
-                ->end()
-                ->scalarNode('response_bin')
-                    ->cannotBeEmpty()
-                    ->defaultValue('%kernel.root_dir%/config/sips/bin/static/response')
-                ->end()
-                ->booleanNode('debug')->defaultValue('%kernel.debug%')->end()
-            ->end()
-        ;
+        $this->addClientSection($treeBuilder->root('ekyna_payum_sips'));
 
         return $treeBuilder;
+    }
+
+    public function addClientSection(ArrayNodeDefinition $node)
+    {
+        $node
+            ->children()
+                ->arrayNode('client')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->scalarNode('merchant_id')->isRequired()->cannotBeEmpty()->end()
+                        ->scalarNode('merchant_country')->isRequired()->cannotBeEmpty()->end()
+                        ->scalarNode('pathfile')
+                            ->cannotBeEmpty()
+                            ->defaultValue('%kernel.root_dir%/sips/param/pathfile')
+                        ->end()
+                        ->scalarNode('request_bin')
+                            ->cannotBeEmpty()
+                            ->defaultValue('%kernel.root_dir%/sips/bin/static/request')
+                        ->end()
+                        ->scalarNode('response_bin')
+                            ->cannotBeEmpty()
+                            ->defaultValue('%kernel.root_dir%/sips/bin/static/response')
+                        ->end()
+                        ->booleanNode('debug')->defaultValue('%kernel.debug%')->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
     }
 }
